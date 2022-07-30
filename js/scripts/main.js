00025c41ac46e468cc3489fa6fbc742139f4293f
Gohara <gohara.junior@dev.auaha.com.br>
2022-07-30 15:38:49 -0300

    dropdown adicionado

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -27,3 +27,16 @@ AOS.init({
     duration: 1000,
     once: true
 });
+
+// Menu Dropdown
+const btnDropdown = document.querySelector('.js-btn-dropdown');
+const dropdown = document.querySelector('.js-dropdown');
+
+function openDropdown(event) {
+    event.preventDefault();
+
+    dropdown.classList.toggle('active');
+}
+
+btnDropdown.addEventListener('click', openDropdown);
+dropdown.addEventListener('mouseleave', openDropdown)
